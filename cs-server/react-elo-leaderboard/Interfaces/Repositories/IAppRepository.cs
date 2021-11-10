ef525248b41b8e8155fc5482f5bce79b6bd82062
Adrian Solheim Almenningen <adrian.s.almenningen@gmail.com>
2021-11-10 13:40:01 +0100

    C# backend HelloWorld

---
+++ cs-server/react-elo-leaderboard/Interfaces/Repositories/IAppRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELO.Leaderboard.Interfaces.Repositories
+{
+    public interface IAppRepository
+    {
+        public string GetHello();
+    }
+}
---
+++ cs-server/react-elo-leaderboard/Src/Controllers/AppController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ELO.Leaderboard.Interfaces.Services;
+
+namespace ELO.Leaderboard.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController : ControllerBase
+    {
+        private readonly IAppService _appService;
+
+        public AppController(IAppService appService)
+        {
+            _appService = appService;
+        }
+
+        [HttpGet()]
+        public IActionResult GetHello()
+        {
+            return Ok(_appService.GetHello());
+        }
+    }
+}
